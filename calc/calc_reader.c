6235570f611abd336e6aa281ecc90e7704b5cdac
MaksimVGU <14maxim4@gmail.com>
2023-03-22 01:32:31 +0300

    fix

--- calc/calc_reader.c
+++ calc/calc_reader.c
@@ -1,105 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
-#include <math.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-#define BUFFER_SIZE 256
+#define MAX_MSG_SIZE 512
 
-struct message {
-    long mtype;
-    char mtext[BUFFER_SIZE];
-};
+typedef struct {
+    long type;
+    char operation;
+    double operand1;
+    double operand2;
+} CalcTask;
 
-void calculate(char *command, char *result)
-{
-    int a, b;
-    char op;
-    sscanf(command, "%d %c %d", &a, &op, &b);
-
-    switch (op)
-    {
-        case '+':
-            sprintf(result, "%d", a + b);
-            break;
-        case '-':
-            sprintf(result, "%d", a - b);
-            break;
-        case '*':
-            sprintf(result, "%d", a * b);
-            break;
-        case '/':
-            if (b == 0) {
-                strcpy(result, "Divide by zero error");
-            } else {
-                sprintf(result, "%d", a / b);
-            }
-            break;
-        case '^':
-            sprintf(result, "%d", (int) pow(a, b));
-            break;
-        default:
-            strcpy(result, "Invalid operator");
-            break;
-    }
-}
-
-int main()
-{
-    key_t key;
-    int msqid;
-    struct message msg;
-    size_t length;
-
-    // generate unique key
-    if ((key = ftok(".", 'c')) == -1) {
+int main() {
+    key_t key = ftok("/tmp", 'C');
+    if (key == -1) {
         perror("ftok");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    // create message queue
-    if ((msqid = msgget(key, 0666 | IPC_CREAT)) == -1) {
+    int msgid = msgget(key, IPC_CREAT | 0666);
+    if (msgid == -1) {
         perror("msgget");
-        exit(EXIT_FAILURE);
+        exit(1);
     }
 
-    // receive messages and process them
-    while (true) {
-        // receive message
-        if (msgrcv(msqid, &msg, BUFFER_SIZE, 0, 0) == -1) {
-            perror("msgrcv");
-            exit(EXIT_FAILURE);
-        }
+    while (1) {
+        CalcTask task;
 
-        // check for end-of-file message
-        if (strcmp(msg.mtext, "EOF") == 0) {
-            break;
-        }
+        printf("Enter an operation (+, -, *, /, ^) and two operands: ");
+        scanf(" %c %lf %lf", &task.operation, &task.operand1, &task.operand2);
 
-        // process message
-        char result[BUFFER_SIZE];
-        calculate(msg.mtext, result);
+        task.type = 1;
 
-        // send result back
-        struct message reply = { 1, "" };
-        strncpy(reply.mtext, result, BUFFER_SIZE);
-        length = strlen(reply.mtext) + 1;
-        if (msgsnd(msqid, &reply, length, 0) == -1) {
+        if (msgsnd(msgid, &task, sizeof(task) - sizeof(long), 0) == -1) {
             perror("msgsnd");
-            exit(EXIT_FAILURE);
+            exit(1);
         }
     }
 
-    // remove message queue
-    if (msgctl(msqid, IPC_RMID, NULL) == -1) {
-        perror("msgctl");
-        exit(EXIT_FAILURE);
-    }
-
-    return EXIT_SUCCESS;
+    return 0;
 }
---
+++ calc/calc_worker.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/msg.h>
+#include <math.h>
+
+#define MSG_KEY 12345
+
+struct msgbuf {
+    long mtype;
+    char mtext[100];
+};
+
+int main() {
+    int msgid;
+    struct msgbuf msg;
+    double result;
+    char op;
+    double a, b;
+
+    // Получаем идентификатор очереди сообщений
+    msgid = msgget(MSG_KEY, 0666 | IPC_CREAT);
+    if (msgid == -1) {
+        perror("msgget");
+        exit(1);
+    }
+
+    printf("Calc Worker started, waiting for tasks...\n");
+
+    while (1) {
+        // Получаем сообщение из очереди сообщений
+        if (msgrcv(msgid, &msg, sizeof(msg.mtext), 1, 0) == -1) {
+            perror("msgrcv");
+            exit(1);
+        }
+
+        // Извлекаем аргументы и операцию из сообщения
+        sscanf(msg.mtext, "%lf%c%lf", &a, &op, &b);
+
+        // Вычисляем результат
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    result = INFINITY;
+                } else {
+                    result = a / b;
+                }
+                break;
+            case '^':
+                result = pow(a, b);
+                break;
+            default:
+                result = NAN;
+                break;
+        }
+
+        // Создаем новое сообщение с результатом вычислений
+        sprintf(msg.mtext, "%lf", result);
+        msg.mtype = 2;
+
+        // Отправляем сообщение обратно в очередь сообщений
+        if (msgsnd(msgid, &msg, sizeof(msg.mtext), 0) == -1) {
+            perror("msgsnd");
+            exit(1);
+        }
+
+        printf("Task completed: %lf %c %lf = %lf\n", a, op, b, result);
+    }
+
+    return 0;
+}
