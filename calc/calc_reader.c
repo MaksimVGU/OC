894541315cde7d5e6487e0acae75076a76f3010f
MaksimVGU <14maxim4@gmail.com>
2023-03-24 14:44:26 +0300

    comments

--- calc/calc_reader.c
+++ calc/calc_reader.c
@@ -7,55 +7,55 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#define MSG_KEY 12345
-#define MAX_MSG_SIZE 512
 
-typedef struct {
-    long type;
-    char operation;
-    double operand1;
-    double operand2;
+#define MSG_KEY 12345 // определение ключа для очереди сообщений
+#define MAX_MSG_SIZE 512 // максимальный размер сообщения в очереди
+
+typedef struct { // определение пользовательской структуры CalcTask
+    long type; // тип сообщения
+    char operation; // операция калькулятора
+    double operand1; // первый операнд
+    double operand2; // второй операнд
 } CalcTask;
 
 int main() 
 {
-    key_t key = ftok("/tmp", 'C');
-    if (key == -1) 
+    key_t key = ftok("/tmp", 'C'); // создание ключа IPC для очереди сообщений
+    if (key == -1) // проверка успешности создания ключа
     {
-        perror("ftok");
-        exit(1);
+        perror("ftok"); // вывод сообщения об ошибке
+        exit(1); // завершение программы с кодом 1
     }
 
-    int msgid = msgget(MSG_KEY, 0666 | IPC_CREAT);
-    if (msgid == -1) 
+    int msgid = msgget(MSG_KEY, 0666 | IPC_CREAT); // создание или подключение к очереди сообщений
+    if (msgid == -1) // проверка успешности создания очереди
     {
-        perror("msgget");
-        exit(1);
+        perror("msgget"); // вывод сообщения об ошибке
+        exit(1); // завершение программы с кодом 1
     }
 
-    while (1) 
+    while (1) // бесконечный цикл для ожидания и обработки задач калькулятора
     {
-        CalcTask task;
+        CalcTask task; // создание экземпляра структуры CalcTask
 
-        printf("Enter an operation (+, -, *, /, ^) and two operands:");
-        scanf(" %c %lf %lf", &task.operation, &task.operand1, &task.operand2);
+        printf("Enter an operation (+, -, *, /, ^) and two operands:"); // запрос на ввод операции и двух операндов
+        scanf(" %c %lf %lf", &task.operation, &task.operand1, &task.operand2); // считывание введенных значений и сохранение в экземпляре структуры CalcTask
 
-        char msg[MAX_MSG_SIZE];
-        sprintf(msg, "%lf %c %lf", task.operand1, task.operation, task.operand2);
+        char msg[MAX_MSG_SIZE]; // создание буфера для сообщения в очереди
+        sprintf(msg, "%lf %c %lf", task.operand1, task.operation, task.operand2); // формирование сообщения вбуфере msg в формате: "значение_операнда1 операция значение_операнда2"
+        task.type = 1; // установка типа сообщения в структуре CalcTask
 
-        task.type = 1;
-
-        if (msgsnd(msgid, msg, strlen(msg) + 1, 0) == -1) 
+        if (msgsnd(msgid, msg, strlen(msg) + 1, 0) == -1) // отправка сообщения в очередь
         {
-            perror("msgsnd");
-            exit(1);
+            perror("msgsnd"); // вывод сообщения об ошибке
+            exit(1); // завершение программы с кодом 1
         }
-        if (msgsnd(msgid, &task, sizeof(task) - sizeof(long), 0) == -1) 
+        if (msgsnd(msgid, &task, sizeof(task) - sizeof(long), 0) == -1) // отправка сообщения в очередь
         {
-            perror("msgsnd");
-            exit(1);
+            perror("msgsnd"); // вывод сообщения об ошибке
+            exit(1); // завершение программы с кодом 1
         }
     }
 
-    return 0;
+    return 0; 
 }
