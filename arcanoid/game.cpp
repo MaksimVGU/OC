6465db3193b2df9ac2e43265e30b18e9f9f5e8c2
MaksimVGU <14maxim4@gmail.com>
2023-05-07 03:18:22 +0300

    123

--- arcanoid/game.cpp
+++ arcanoid/game.cpp
@@ -1,31 +1,176 @@
-#include <iostream> 
-#include <cstdlib>
-#include <ctime>
+#include <iostream>
+#include <cstring>
 
-using namespace std; // использование пространства имен std
+using namespace std;
 
-int main() 
-{ 
-    srand(time(0)); // инициализация генератора случайных чисел
-    int randomNumber = rand() % 10 + 1; // генерация случайного числа от 1 до 10
-    int guess; // определение переменной для ввода догадки игрока
-    int tries = 0; // определение переменной для подсчета количества попыток
+const int BOARD_SIZE = 3;
 
-    cout << "Добро пожаловать в игру 'Угадай число'!" << endl; // вывод приветствия
+char board[BOARD_SIZE][BOARD_SIZE];
 
-    do { // начало цикла do-while
-        cout << "Введите число от 1 до 10: "; // вывод приглашения на ввод догадки
-        cin >> guess; // чтение догадки игрока из консоли
-        tries++; // увеличение количества попыток на 1
-
-        if (guess > randomNumber) { // проверка догадки игрока на больше, чем случайное число
-            cout << "Слишком большое число! Попробуйте еще раз." << endl; // вывод сообщения о том, что число слишком большое
-        } else if (guess < randomNumber) { // проверка догадки игрока на меньше, чем случайное число
-            cout << "Слишком маленькое число! Попробуйте еще раз." << endl; // вывод сообщения о том, что число слишком маленькое
-        } else { // если догадка игрока совпадает со случайным числом
-            cout << endl << "Поздравляю! Вы угадали число за " << tries << " попыток!" << endl; // вывод сообщения о победе и количестве попыток
+void initBoard()
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            board[i][j] = '-';
         }
-    } while (guess != randomNumber); // конец цикла do-while, продолжать, пока догадка игрока не совпадет со случайным числом
-
-    return 0; // возврат нуля, что указывает на успешное завершение программы
+    }
 }
+
+void printBoard()
+{
+    cout << "  1 2 3" << endl;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        cout << i + 1 << " ";
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool isBoardFull()
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] == '-')
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool isWinner(char player)
+{
+    // Проверка строк
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        bool win = true;
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[i][j] != player)
+            {
+                win = false;
+            }
+        }
+        if (win)
+        {
+            return true;
+        }
+    }
+
+    // Проверка столбцов
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        bool win = true;
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            if (board[j][i] != player)
+            {
+                win = false;
+            }
+        }
+        if (win)
+        {
+            return true;
+        }
+    }
+
+    // Проверка диагоналей
+    bool win = true;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        if (board[i][i] != player)
+        {
+            win = false;
+        }
+    }
+    if (win)
+    {
+        return true;
+    }
+
+    win = true;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        if (board[i][BOARD_SIZE - i - 1] != player)
+        {
+            win = false;
+        }
+    }
+    if (win)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    initBoard();
+    char currentPlayer = 'X';
+
+    while (true)
+    {
+        printBoard();
+
+        // Получаем ход игрока
+        int row, col;
+        cout << "Ход игрока " << currentPlayer << ". Введите номер строки и столбца: ";
+        cin >> row >> col;
+
+        // Проверяем, что выбранные координаты действительны
+        if (row < 1 || row > BOARD_SIZE || col < 1 || col > BOARD_SIZE)
+        {
+            cout << "Неверные координаты. Попробуйте еще раз." << endl;
+            continue;
+        }
+
+        // Проверяем, что выбранная клетка не занята
+        if (board[row - 1][col - 1] != '-')
+        {
+            cout << "Эта клетка уже занята. Попробуйте еще раз." << endl;
+            continue;
+        }
+
+        // Выполняем ход
+        board[row - 1][col - 1] = currentPlayer;
+
+        // Проверяем, есть ли победитель
+        if (isWinner(currentPlayer))
+        {
+            cout << "Игрок " << currentPlayer << " победил!" << endl;
+            break;
+        }
+
+        // Проверяем, заполнена ли доска
+        if (isBoardFull())
+        {
+            cout << "Ничья!" << endl;
+            break;
+        }
+
+        // Меняем игрока
+        if (currentPlayer == 'X')
+        {
+            currentPlayer = 'O';
+        }
+        else
+        {
+            currentPlayer = 'X';
+        }
+    }   
+
+    printBoard();
+
+    return 0;
+}
+
